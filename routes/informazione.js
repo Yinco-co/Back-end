639369e0eb4eb98b058e05c7491f92952b377674
AsaiAshita <114286827+AsaiAshita@users.noreply.github.com>
2022-12-24 23:01:06 +0100

    Update informazione.js

--- routes/informazione.js
+++ routes/informazione.js
@@ -4,17 +4,5 @@ const router = express.Router();
 const infoController = require('../controllers/informazione');
 
 router.get('/?', infoController.getOneInfo);
-router.get('/', async (req, res) =>{
-    let books = await informazione.find({});
-    books = books.map( (informazione) =>{
-        return {
-            self: informazione.title,
-            year: informazione.year,
-            body: informazione.body,
-            tags: informazione.tags
-        };
-        });
-        res.status(200).json(books);
-    });
 
 module.exports = router; // export to use in server.js

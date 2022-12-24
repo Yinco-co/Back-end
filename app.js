c289c905cd5f568ce8631ac50d915d990f76f34d
AsaiAshita <114286827+AsaiAshita@users.noreply.github.com>
2022-12-24 09:47:01 +0100

    Modifiche per testing - L

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 require('dotenv').config()
+const express = require('express');
+const app = express();
+const routes = require('./routes/docente.js')//(app, {});
+const path = require('path');
+const multer = require ('multer');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
-console.log("new app");
-console.log(process.env.DB_HOST)
-console.log(process.env.DB_USER)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(express.json());
+app.use(multer().none());
+app.use(express.urlencoded({extended:false}));
+
+app.use('/docente', require('./routes/docente'));
+
+app.get('/api', (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.use('/damn', require('./routes/informazione'));
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,25 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
-const app = express();
-const routes = require('./routes/docente.js')//(app, {});
+const app = require('./app.js');
 const path = require('path');
-const multer = require ('multer');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(express.json());
-app.use(multer().none());
-app.use(express.urlencoded({extended:false}));
-
-app.use('/docente', require('./routes/docente'));
-
-app.get('/api', (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
-app.use('/damn', require('./routes/informazione'));
+const dotenv = require('dotenv').config();
 
 mongoose.connect(
   process.env.MONGODB_URI,

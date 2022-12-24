8348dbb3648d3c5f5b95f2c8fef20102616d09a5
Daniele Notarangelo <50075274+danielenotarangelo@users.noreply.github.com>
2022-12-24 11:35:37 +0100

    Add files via upload

--- test/docente.test.js
+++ test/docente.test.js
@@ -1,8 +1,15 @@
-const { getOneDocente } = require('../controllers/docente');
-const controller = require('./controllers/docente');
+const request = require('supertest');
+const app = require('../app');
 
-describe('test-coverage per le funzioni del controller docente', () =>{
-test('See if the functions works as intended', () => {
-    expect(getOneDocente('Passerone')).toBe('https://webapps.unitn.it/du/it/Persona/PER0000817/Curriculum');
-})
+describe('Test dell\'API Docente', () => {
+  test('Deve restituire il docente con il cognome specificato', async () => {
+    const res = await request(app).get('/docente?cognome=Passerone');
+    expect(res.statusCode).toEqual(200);
+    expect(response.body.url).toEqual({ url: 'https://webapps.unitn.it/du/it/Persona/PER0000817/Curriculum' });
+  }, 20000);
+
+  test('Deve restituire un errore se il docente non esiste', async () => {
+    const res = await request(app).get('/docente?cognome=Bianchi');
+    expect(res.statusCode).toEqual(404);
+  }, 20000);
 });

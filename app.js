1bbd9e4aaa525d87b32b1f259c8022f4c1b6cc1c
Zakka02 <107118027+Zakka02@users.noreply.github.com>
2022-12-24 23:22:21 +0100

    update test e app.js

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const multer = require ('multer');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
+const mongoose = require('mongoose');
+mongoose.connect(
+  process.env.MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+);
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 app.use(multer().none());
--- routes/docente.js
+++ routes/docente.js
@@ -5,15 +5,4 @@ const docenteController = require('../controllers/docente');
 
 router.get('/?', docenteController.getOneDocente);
 
-/*router.get('/', async (req, res) =>{
-    let books = await docente.find({});
-    books = books.map( (docente) =>{
-        return {
-            self: docente.cognome,
-            link: docente.url
-        };
-        });
-        res.status(200).json(books);
-    });
-*/
 module.exports = router; // export to use in server.js
